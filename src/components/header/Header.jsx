1975117ecdabc96eb19a954ab6dc101bb3b10e35
sushma yadav <sushma.yadav@mounttalent.com>
2023-01-17 17:29:00 +0530

    logout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,25 @@ import styles from "./header.module.css";
 import { useState } from "react";
 import logo from "../../assets/images/myntralogo.png"
 import { Link } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-function Header() {
+function Header(props) {
     const linkItems = [{ name: "MEN", url: '/men' }, { name: "WOMEN", url: '/women' }, { name: "KIDS", url: '/kids' }, { name: "HOME & LIVING", url: '/home-living' }, { name: "BEAUTY", url: '/beauty' }, { name: "STUDIO", url: '/studio' }]
-
     const [showToggle, setShowToggle] = useState(false)
+    const [showDropdown, setShowDropdown] = useState(false)
+    const navigate = useNavigate()
+
+    const handleSignOut = (e) => {
+        e.preventDefault()
+        const auth = getAuth();
+        signOut(auth).then((res) => {
+            console.log(res)
+            navigate("/")
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
 
     return (
         <div className={styles.header}>
@@ -49,7 +63,7 @@ function Header() {
                         <input type="text" placeholder="Seach for products, brands and more" />
                     </div>
                     <div className={styles.headerIcons}>
-                        <Link to="/login" className={styles.icons}>
+                        <Link to="/login" className={styles.icons} style={props.userName ? { display: 'none' } : { display: "visile" }}>
                             <i className="far fa-user"></i>
                             <p>Profile</p>
                         </Link>
@@ -61,6 +75,15 @@ function Header() {
                             <i className="fa fa-shopping-cart" aria-hidden="true"></i>
                             <p>Bag</p>
                         </Link>
+                        {props.userName && <div className={styles.icons}>
+                            <div className={styles.profile} onClick={() => setShowDropdown(!showDropdown)}>
+                                <p className={styles.profileInitial}>{props.userName.charAt(0).toUpperCase()}</p>
+                            </div>
+                            {showDropdown && <div className={styles.logout} onClick={handleSignOut}>
+                                <div style={{ display: "flex", gap: "10px" }}><i className="far fa-sign-out"></i>
+                                    <p>Logout</p></div>
+                            </div>}
+                        </div>}
                     </div>
                 </div>
             </div>
